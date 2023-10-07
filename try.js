f60a9c2f0a1ebc1fdfff77b380d14adbb64727eb
christine <christine.muriungi@student.moringaschool.com>
2023-10-07 20:55:51 +0300

    completed assignment

---
+++ try.js
@@ -0,0 +1,18 @@
+
+function scuberGreetingForFeet(feet){
+    let scuberGreetingForFeet;
+    if (feet <= 400){
+    console.log("free");
+  }else
+      if (feet = 199){
+      console.log("This one is on me!");
+    }
+   else if (feet > 2000){
+   console.log("charges 30 dollars");
+   } else if(feet > 2500){
+    console.log("does not allow ride over 2500 feet");
+   }
+  }
+
+  let feet = (400);
+  console.log("scuberGreetingForFeet");
